2b749e5d4e55a62acc7fabc46d00ca57f86d310e
Azwad Islam <azwad.sachcha@gmail.com>
2024-11-11 02:25:04 +0600

    First push IDS

---
+++ Lab-2-classPractice.R
@@ -0,0 +1,46 @@
+1+1
+2+1
+2+3*4
+3^2
+sqrt(10)
+pi
+
+x <- 10
+y <- 9
+x+y #jhgjkgh
+x
+x<-x+y
+x
+x<-9.01
+floor (x)
+ceiling(x)
+a<--10
+abs(a)
+sqrt(x)
+x<-"string"
+b<-"hello world"
+toupper(x)
+strsplit(x,split = "")
+a<-strsplit(x,split = "")
+strsplit(b,split = " ")
+a
+nchar(x)
+paste(x,b)
+nchar(b)
+strsplit(x,split = "i")
+paste0(x,b)
+paste(x,b,sep = "")
+cat(x,b)
+cat(x)
+str<-"hello world hello hello
+ohioiioioi"
+str
+print(str)
+cat(str)
+
+
+a<-20
+b<-15
+if(a>b){
+  cat("a is greater")
+}else{cat("a is not greater")
