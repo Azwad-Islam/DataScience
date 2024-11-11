2e81a0561616fed943ba72f7b17abba1e5db93f2
Azwad Islam <72523235+Azwad-Islam@users.noreply.github.com>
2024-11-11 09:48:03 +0600

    lab4 Csv function

---
+++ Lab-4-classPractice.R
@@ -0,0 +1,20 @@
+#readline function
+
+var1 = readline(prompt = "enter any value: ")
+Var2= readline(prompt = "enter any number: ")
+
+var2= as.integer(Var2)
+
+print(var1)
+print(var2)
+
+
+#scan function
+x = scan()
+print(paste(x, "is best")) #paste for string and variable
+
+d= scan(what = "")#not checked
+
+
+
+
