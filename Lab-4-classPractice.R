0cd4de5f496283a4ea6af79952c784f1fde3ef5b
Azwad Islam <72523235+Azwad-Islam@users.noreply.github.com>
2024-11-11 10:23:53 +0600

    importing csv

--- Lab-4-classPractice.R
+++ Lab-4-classPractice.R
@@ -15,6 +15,14 @@ print(paste(x, "is best")) #paste for string and variable
 
 d= scan(what = "")#not checked
 
+#edit function
+mydata<-data.frame(age=numeric(0),gender = character(0),weight = numeric(0))
+mydata <- edit(mydata)
 
+#csv file function
+write.csv(mydata, "A:/IDS/DataScience/lab4Mydata.csv")
 
-
+#importing csv file
+irisDataSet<-read.csv("A:/IDS/DataScience/Iris.csv",header = TRUE, sep = ",")
+irisDataSet
+edit(irisDataSet)
